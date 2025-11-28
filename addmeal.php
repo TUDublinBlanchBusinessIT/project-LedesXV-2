541bf426c8b1988b0c9976d10462940c53edd81d
LedesXV <b00164804@mytudublin.ie>
2025-11-28 14:05:24 +0000

    Added the meal form and connected to index

---
+++ addmeal.php
@@ -0,0 +1,39 @@
+
+
+<h2>Add a Meal</h2>
+
+<form action="insertmeal.php" method="POST">
+
+    <label>Meal Type</label>
+    <input type="text" name="meal_type" required>
+
+    <br><br>
+
+    <label>Calories</label>
+    <input type="number" name="calories" required>
+
+    <br><br>
+
+    <label>Protein</label>
+    <input type="number" name="protein" required>
+
+    <br><br>
+
+    <label>Fat</label>
+    <input type="number" name="fat" required>
+
+    <br><br>
+
+    <label>Carbs</label>
+    <input type="number" name="carbs" required>
+
+    <br><br>
+
+    <label>Date</label>
+    <input type="date" name="meal_date" required>
+
+    <br><br>
+
+    <button type="submit">Save Meal</button>
+
+</form>
---
+++ index.php
@@ -0,0 +1,1 @@
+<a href="addmeal.php">Add Meal</a>
